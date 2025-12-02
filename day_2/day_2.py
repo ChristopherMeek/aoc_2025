b7eaf4dc3c15fc878f0742dabd118a3888360be9
ChristopherMeek <chris@sensemaking.uk.com>
2025-12-02 12:38:25 +0000

    _

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -2,14 +2,16 @@ from get_aoc_data import get_aoc_data
 from .parser import ranges_parser
 from .solutions import part_1, part_2
 
-test_input = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'
-puzzle_input = get_aoc_data(2)
+TEST_INPUT = '11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'
 
-parsed_test_input = ranges_parser.parse(test_input)
-parsed_puzzle_input = ranges_parser.parse(puzzle_input)
+if __name__ == "__main__":
+    puzzle_input = get_aoc_data(2)
 
-print("Test Input Results:", part_1(parsed_test_input))
-print("Puzzle Input Results:", part_1(parsed_puzzle_input))
+    parsed_test_input = ranges_parser.parse(TEST_INPUT)
+    parsed_puzzle_input = ranges_parser.parse(puzzle_input)
 
-print("Test Input Part 2 Results:", part_2(parsed_test_input))
-print("Puzzle Input Part 2 Results:", part_2(parsed_puzzle_input))
+    print("Part 1 - Test Input:", part_1(parsed_test_input))
+    print("Part 1 - Puzzle Input:", part_1(parsed_puzzle_input))
+
+    print("Part 2 - Test Input:", part_2(parsed_test_input))
+    print("Part 2 - Puzzle Input:", part_2(parsed_puzzle_input))
--- day_2/solutions.py
+++ day_2/solutions.py
@@ -1,44 +1,71 @@
 __all__ = ["part_1", "part_2"]
 
-def part_1(parsed_input):
-    """Find IDs where the first half of digits equals the second half and return their sum."""
+def part_1(parsed_input: list[tuple[int, int]]) -> int:
+    """Find numbers where first half of digits equals second half.
+    
+    Args:
+        parsed_input: List of (start, end) range tuples
+    
+    Returns:
+        Sum of all numbers with repeating halves
+    """
     repeating_ids = []
 
     for start, end in parsed_input:
-        id_range = list(range(start, end + 1))
-
-        for id in id_range:
-            str_id = str(id)
-            if len(str_id) % 2 == 0 and has_repeat_of_length(str_id, len(str_id) // 2):
-                repeating_ids.append(id)
+        for num in range(start, end + 1):
+            str_num = str(num)
+            # Only even-length numbers can have equal halves
+            if len(str_num) % 2 == 0 and has_repeat_of_length(str_num, len(str_num) // 2):
+                repeating_ids.append(num)
     
     return sum(repeating_ids)
 
-def part_2(parsed_input):
+def part_2(parsed_input: list[tuple[int, int]]) -> int:
+    """Find numbers that consist of repeated segments of any length.
+    
+    Args:
+        parsed_input: List of (start, end) range tuples
+    
+    Returns:
+        Sum of unique numbers with any repeating pattern
+    """
     repeating_ids = []
 
     for start, end in parsed_input:
-        id_range = list(range(start, end + 1))
-
-        for id in id_range:
-            str_id = str(id)
-            if(len(str_id) == 1): 
+        for num in range(start, end + 1):
+            str_num = str(num)
+            # Single-digit numbers don't have repeating segments
+            if len(str_num) == 1:
                 continue
             
-            for length in range(1, len(str_id)//2 + 1):
-                if len(str_id) % length == 0 and has_repeat_of_length(str_id, length):
-                    #print(f"Found repeating ID: {id} with segment length {length}")
-                    repeating_ids.append(id)
+            # Check all possible segment lengths from 1 to half the string length
+            for length in range(1, len(str_num) // 2 + 1):
+                if has_repeat_of_length(str_num, length):
+                    repeating_ids.append(num)
+                    break  # Found a repeating pattern, no need to check other lengths
     
     return sum(set(repeating_ids))
-    
 
 def has_repeat_of_length(s: str, length: int) -> bool:
+    """Check if string consists of repeated segments of given length.
+    
+    Args:
+        s: String to check
+        length: Length of each segment
+    
+    Returns:
+        True if all segments are identical, False otherwise
+    
+    Example:
+        has_repeat_of_length("abcabc", 3) -> True
+        has_repeat_of_length("abcdef", 3) -> False
+    """
     if len(s) % length != 0:
         return False
     
-    # Comprehension: create segments s[i,i+length] all the way to the end of s, skipping length as we go
-    # eg. "abcdefgh" -> "ab", "cd", "ef", "gh" for length=2
+    # Create segments: split string into chunks of given length
+    # e.g., "abcdefgh" with length=2 -> ["ab", "cd", "ef", "gh"]
     segments = [s[i:i+length] for i in range(0, len(s), length)]
 
+    # If all segments are identical, set will have only 1 unique element
     return len(set(segments)) == 1
